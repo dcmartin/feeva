0bd3734e4e1350075ec80709cf97f685db281945
dcmartin <github@dcmartin.com>
2020-04-03 18:33:02 -0700

    lgtm

--- src/oled/python/server.sh
+++ src/oled/python/server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+if [ "${USER:-}" != 'root' ]; then echo "Run as root; sudo ${0} ${*}" &> /dev/stderr; exit 1; fi
+
 pids=($(ps alxwww | egrep server.py | egrep -v grep | awk '{ print $1 }'))
-if [ ${#pids[@]} -gt 0 ]; then sudo kill -9 ${pids[@]}; fi
-sudo python3 server.py &> server.out &
-sleep 10
+if [ ${#pids[@]} -gt 0 ]; then kill -9 ${pids[@]}; fi
+python3 server.py &
+echo "Testing; curl -v -d @test.json 127.0.0.1:7777/oled/v1/display/picture" &> /dev/stderr
 curl -v -d @test.json 127.0.0.1:7777/oled/v1/display/picture
--- src/oled/python/setup.sh
+++ src/oled/python/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+if [ "${USER:-}" != 'root' ]; then echo "Run as root; sudo ${0} ${*}" &> /dev/stderr; exit 1; fi
+
 apt update -qq -y
 apt install -qq -y python3-pip libopenjp2-7 libtiff5
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade RPi.GPIO
 python3 -m pip install --upgrade spidev
 python3 -m pip install --upgrade Pillow
+python3 -m pip install --upgrade flask
