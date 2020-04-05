a95b70fe3b1dde1a33086f9b5b0107f8d506b344
dcmartin <github@dcmartin.com>
2020-04-04 18:22:22 -0700

    lgtm

--- src/oled/python/server.sh
+++ src/oled/python/server.sh
@@ -4,5 +4,4 @@ if [ "${USER:-}" != 'root' ]; then echo "Run as root; sudo ${0} ${*}" &> /dev/st
 pids=($(ps alxwww | egrep server.py | egrep -v grep | awk '{ print $1 }'))
 if [ ${#pids[@]} -gt 0 ]; then kill -9 ${pids[@]}; fi
 python3 server.py &
-echo "Testing; curl -v -d @test.json 127.0.0.1:7777/oled/v1/display/picture" &> /dev/stderr
-curl -v -d @test.json 127.0.0.1:7777/oled/v1/display/picture
+echo "Starting; run the following to test: curl -v -d @test.json 127.0.0.1:7777/oled/v1/display/picture" &> /dev/stderr
